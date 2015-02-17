f1e215a0442343673d00ac8e3dd5f80d78efd292
Rhoxio <rhoxiodbc@gmail.com>
2015-02-16 21:54:15 -0800

    Added battle page for testing. Beginning UI design comes next...

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -5,10 +5,12 @@ angular.module('myApp.view2', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'BattleController'
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('BattleController', ['$scope', '$http', function($scope, $http) {
+
+	
 
 }]);

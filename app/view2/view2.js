886bc54be725a3d7cfbaf799876f239db14f83cd
Rhoxio <rhoxiodbc@gmail.com>
2015-02-17 17:04:52 -0800

    Set up promises for retrieving user and enemy information.

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,8 +9,37 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('BattleController', ['$scope', '$http', function($scope, $http) {
+.controller('BattleController', ['$scope', '$http', '$q', function($scope, $http, $q) {
 
-	
+	var characters = []
+	var enemies = []
+
+	var getCharacterData = function(){
+		$http.get('http://localhost:9393/characters').then(function(response){
+			characters.push(response.data)
+			console.log(characters)
+			return response
+		})
+	}
+
+	var getEnemyData = function(){
+		$http.get('http://localhost:9393/enemies').then(function(response){
+			enemies.push(response.data)
+			console.log(enemies)
+			return response
+		})
+	}
+
+	var setUpBattleEnvironment = function(){
+		getEnemyData();
+		getCharacterData();
+
+		$('.thing').on('click', function(){
+			$('.thing').css('background-color', 'red')
+		})
+	}
+
+	setUpBattleEnvironment()
+
 
 }]);

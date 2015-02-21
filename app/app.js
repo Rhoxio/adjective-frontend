b7ce65b75175ee107a3d09dff5d9b9167da9ab74
Rhoxio <rhoxiodbc@gmail.com>
2015-02-20 18:34:52 -0800

    Renamed the partials and controllers from their defaults.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
-		this.characters = 1
-
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
   'myApp.creation',
-  'myApp.view2',
+  'myApp.battle',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
--- app/battle/battle.js
+++ app/battle/battle.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.battle', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
+  $routeProvider.when('/battle', {
+    templateUrl: 'battle/battle.html',
     controller: 'BattleController'
   });
 }])
